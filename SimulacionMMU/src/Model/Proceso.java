fc25b75d6a95bb4bf5a76da9a4152812e4b17693
Jefry99XD <jeffryfredo@gmail.com>
2023-10-14 15:44:03 -0600

    avance proceso

---
+++ SimulacionMMU/src/Model/Proceso.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author jeffr
+ */
+public class Proceso {
+    private int pid;
+    private int size;
+    
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getPid() {
+        return pid;
+    }
+    
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
+    /*
+    public void New(int pid, int size){
+        this.setPid(pid);
+        this.setSize(size);
+    }
+    public void delete(int pid, int size){
+        
+    }
+    public void kill(int pid, int size){
+        
+    }
+    public void use(int pid, int size){
+        
+    }
+    */
+    
+}
